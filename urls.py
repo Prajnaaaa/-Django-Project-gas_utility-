83dc3528cc48f04cbb903359796176f6e23977a3
Prajna Majumdar <100031258+Prajnaaaa@users.noreply.github.com>
2025-03-27 20:42:54 +0530

    urls.py
    customer_service/urls.py

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('submit/', views.submit_request, name='submit_request'),
+    path('track/', views.track_requests, name='track_requests'),
+    path('request/<int:request_id>/', views.request_detail, name='request_detail'),
+]
